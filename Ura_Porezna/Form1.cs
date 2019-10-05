aa1726a497f5d73e7e29081d1659b9e65afa5fb5
ivek81cro <ivek81cro@gmail.com>
2019-10-05 21:46:55 +0200

    fix

--- Ura_Porezna/Form1.cs
+++ Ura_Porezna/Form1.cs
@@ -597,12 +597,17 @@ namespace Ura_Porezna
             }
             /*Poruka poruka = new Poruka();
             poruka.Show();*/
-            dataGridView1.ColumnCount = 45;
-            for (int i = 0; i < 45; i++)
-            {
-
-                dataGridView1.Columns[i].HeaderText = i.ToString();
-            }
+            string constring = "datasource=localhost;port=3306;username=root;password=pass123";
+            MySqlConnection con = new MySqlConnection(constring);
+            string query = "INSERT INTO poreznaura.ura (Rbr, Datum_racuna, Broj_racuna, Za_uplatu, " +
+                "Naziv_dobavljaca, Sjediste_dobavljaca, OIB, Iznos_s_porezom, Porezna_osn0, " +
+                "Porezna_osn5, Porezna_osn13, Porezna_osn25, Ukupni_pretporez, por5, por13, por25, " +
+                "br_primke, storno, odobr) " +
+                "VALUES (@Rbr, @Datum_racuna, @Broj_racuna, @Za_uplatu, @Naziv_dobavljaca, " +
+                "@Sjediste_dobavljaca, @OIB, @Iznos_s_porezom, @Porezna_osn0, @Porezna_osn5, " +
+                "@Porezna_osn13, @Porezna_osn25, @Ukupni_pretporez, @por5, @por13, @por25, " +
+                "@br_primke, @storno, @odobr);";
+            con.Open();
             try
             {
                 string[] lines = File.ReadAllLines(put);
@@ -614,7 +619,29 @@ namespace Ura_Porezna
                     DateTime dt1 = DateTime.Parse(text[2]);
                     text[6] = dt1.ToString("yyyy-MM-dd");
 
-                    dataGridView1.Rows.Add(text);
+                    MySqlCommand cmd = new MySqlCommand(query, con);
+
+                    cmd.Parameters.AddWithValue("@Rbr", text[1]);
+                    cmd.Parameters.AddWithValue("@Datum_racuna", text[6].ToString());
+                    cmd.Parameters.AddWithValue("@Broj_racuna", text[3].ToString().Trim());
+                    cmd.Parameters.AddWithValue("@Za_uplatu", Convert.ToDouble(text[11].ToString().Trim()));
+                    cmd.Parameters.AddWithValue("@Naziv_dobavljaca", text[12].ToString().Trim());
+                    cmd.Parameters.AddWithValue("@br_primke", Convert.ToDouble(text[13].ToString().Trim()));
+                    cmd.Parameters.AddWithValue("@Sjediste_dobavljaca", text[16].ToString().Trim());
+                    cmd.Parameters.AddWithValue("@OIB", text[17].ToString().Trim());
+                    cmd.Parameters.AddWithValue("@Iznos_s_porezom", Convert.ToDouble(text[18].ToString().Trim()));
+                    cmd.Parameters.AddWithValue("@Porezna_osn0", Convert.ToDouble(text[19].ToString().Trim()));
+                    cmd.Parameters.AddWithValue("@Porezna_osn5", Convert.ToDouble(text[20].ToString().Trim()));
+                    cmd.Parameters.AddWithValue("@Porezna_osn13", Convert.ToDouble(text[24].ToString().Trim()));
+                    cmd.Parameters.AddWithValue("@Porezna_osn25", Convert.ToDouble(text[28].ToString().Trim()));
+                    cmd.Parameters.AddWithValue("@Ukupni_pretporez", Convert.ToDouble(text[30].ToString().Trim()));
+                    cmd.Parameters.AddWithValue("@por5", Convert.ToDouble(text[21].ToString().Trim()));
+                    cmd.Parameters.AddWithValue("@por13", Convert.ToDouble(text[25].ToString().Trim()));
+                    cmd.Parameters.AddWithValue("@por25", Convert.ToDouble(text[29].ToString().Trim()));
+                    cmd.Parameters.AddWithValue("@storno", text[5].ToString().Trim());
+                    cmd.Parameters.AddWithValue("@odobr", text[37].ToString().Trim());
+
+                    cmd.ExecuteNonQuery();
                 }
             }
             catch
@@ -623,60 +650,13 @@ namespace Ura_Porezna
                     "(zatvori calc ili excel)");
                 return;
             }
+            con.Close();
 
-            foreach (DataGridViewRow row in dataGridView1.Rows)
-            {
-                try
-                {
-                    string constring = "datasource=localhost;port=3306;username=root;password=pass123";
-                    MySqlConnection con = new MySqlConnection(constring);
-                    string query = "INSERT INTO poreznaura.ura (Rbr, Datum_racuna, Broj_racuna, Za_uplatu, " +
-                        "Naziv_dobavljaca, Sjediste_dobavljaca, OIB, Iznos_s_porezom, Porezna_osn0, " +
-                        "Porezna_osn5, Porezna_osn13, Porezna_osn25, Ukupni_pretporez, por5, por13, por25, " +
-                        "br_primke, storno, odobr) " +
-                        "VALUES (@Rbr, @Datum_racuna, @Broj_racuna, @Za_uplatu, @Naziv_dobavljaca, " +
-                        "@Sjediste_dobavljaca, @OIB, @Iznos_s_porezom, @Porezna_osn0, @Porezna_osn5, " +
-                        "@Porezna_osn13, @Porezna_osn25, @Ukupni_pretporez, @por5, @por13, @por25, " +
-                        "@br_primke, @storno, @odobr);";
-
-                    MySqlCommand cmd = new MySqlCommand(query, con);
-
-                    if (row.IsNewRow) continue;
-                    if (row.Cells[0].Value.ToString() == "Rbr") continue;
-                    if (row.Cells[0].Value.ToString() == "") continue;
-
-                    cmd.Parameters.AddWithValue("@Rbr", row.Cells[1].Value);
-                    cmd.Parameters.AddWithValue("@Datum_racuna", row.Cells[6].Value.ToString());
-                    cmd.Parameters.AddWithValue("@Broj_racuna", row.Cells[3].Value.ToString().Trim());
-                    cmd.Parameters.AddWithValue("@Za_uplatu", Convert.ToDouble(row.Cells[11].Value.ToString().Trim()));
-                    cmd.Parameters.AddWithValue("@Naziv_dobavljaca", row.Cells[12].Value.ToString().Trim());
-                    cmd.Parameters.AddWithValue("@br_primke", Convert.ToDouble(row.Cells[13].Value.ToString().Trim()));
-                    cmd.Parameters.AddWithValue("@Sjediste_dobavljaca", row.Cells[16].Value.ToString().Trim());
-                    cmd.Parameters.AddWithValue("@OIB", row.Cells[17].Value.ToString().Trim());
-                    cmd.Parameters.AddWithValue("@Iznos_s_porezom", Convert.ToDouble(row.Cells[18].Value.ToString().Trim()));
-                    cmd.Parameters.AddWithValue("@Porezna_osn0", Convert.ToDouble(row.Cells[19].Value.ToString().Trim()));
-                    cmd.Parameters.AddWithValue("@Porezna_osn5", Convert.ToDouble(row.Cells[20].Value.ToString().Trim()));
-                    cmd.Parameters.AddWithValue("@Porezna_osn13", Convert.ToDouble(row.Cells[24].Value.ToString().Trim()));
-                    cmd.Parameters.AddWithValue("@Porezna_osn25", Convert.ToDouble(row.Cells[28].Value.ToString().Trim()));
-                    cmd.Parameters.AddWithValue("@Ukupni_pretporez", Convert.ToDouble(row.Cells[30].Value.ToString().Trim()));
-                    cmd.Parameters.AddWithValue("@por5", Convert.ToDouble(row.Cells[21].Value.ToString().Trim()));
-                    cmd.Parameters.AddWithValue("@por13", Convert.ToDouble(row.Cells[25].Value.ToString().Trim()));
-                    cmd.Parameters.AddWithValue("@por25", Convert.ToDouble(row.Cells[29].Value.ToString().Trim()));
-                    cmd.Parameters.AddWithValue("@storno", row.Cells[5].Value.ToString().Trim());
-                    cmd.Parameters.AddWithValue("@odobr", row.Cells[37].Value.ToString().Trim());
-
-                    con.Open();
-                    cmd.ExecuteNonQuery();
-                    con.Close();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
-            }
-            //poruka.Close();
+            IspisIzBaze();
+            
             MessageBox.Show("Unešeno");
         }
+
         //kreiraj xml
         private void button1_Click(object sender, EventArgs e)
         {
