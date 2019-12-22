fcf9239dd35ff19aa79302e52de938bc3677da0a
ivek81cro <ivek81cro@gmail.com>
2019-12-22 23:32:52 +0100

    Remove outdated instructions

--- Ura_Porezna/URA/FormURA.Designer.cs
+++ Ura_Porezna/URA/FormURA.Designer.cs
@@ -31,7 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormURA));
             this.button1 = new System.Windows.Forms.Button();
             this.datumDo = new System.Windows.Forms.DateTimePicker();
-            this.dataGridView1 = new CustomDataGridView();
+            this.dataGridView1 = new Ura_Porezna.CustomDataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -47,10 +47,8 @@
             this.label11 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
-            this.label13 = new System.Windows.Forms.Label();
             this.button5 = new System.Windows.Forms.Button();
             this.label14 = new System.Windows.Forms.Label();
-            this.label15 = new System.Windows.Forms.Label();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.txtDob = new System.Windows.Forms.TextBox();
@@ -75,7 +73,7 @@
             this.datumDo.Format = System.Windows.Forms.DateTimePickerFormat.Short;
             this.datumDo.Location = new System.Drawing.Point(100, 95);
             this.datumDo.Name = "datumDo";
-            this.datumDo.Size = new System.Drawing.Size(122, 22);
+            this.datumDo.Size = new System.Drawing.Size(122, 20);
             this.datumDo.TabIndex = 2;
             this.datumDo.Value = new System.DateTime(2019, 2, 10, 22, 55, 5, 0);
             // 
@@ -100,7 +98,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(304, 152);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(12, 17);
+            this.label1.Size = new System.Drawing.Size(10, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = ".";
             // 
@@ -109,7 +107,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(304, 67);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(12, 17);
+            this.label2.Size = new System.Drawing.Size(10, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = ".";
             // 
@@ -118,7 +116,7 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(304, 97);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(12, 17);
+            this.label3.Size = new System.Drawing.Size(10, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = ".";
             // 
@@ -127,7 +125,7 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(304, 125);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(12, 17);
+            this.label4.Size = new System.Drawing.Size(10, 13);
             this.label4.TabIndex = 7;
             this.label4.Text = ".";
             // 
@@ -136,7 +134,7 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(304, 37);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(12, 17);
+            this.label5.Size = new System.Drawing.Size(10, 13);
             this.label5.TabIndex = 8;
             this.label5.Text = ".";
             // 
@@ -165,7 +163,7 @@
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(18, 97);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(75, 17);
+            this.label6.Size = new System.Drawing.Size(58, 13);
             this.label6.TabIndex = 11;
             this.label6.Text = "Datum Do:";
             // 
@@ -174,7 +172,7 @@
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(18, 69);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(76, 17);
+            this.label7.Size = new System.Drawing.Size(58, 13);
             this.label7.TabIndex = 12;
             this.label7.Text = "Datum Od:";
             // 
@@ -183,7 +181,7 @@
             this.label8.AutoSize = true;
             this.label8.Location = new System.Drawing.Point(453, 37);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(12, 17);
+            this.label8.Size = new System.Drawing.Size(10, 13);
             this.label8.TabIndex = 13;
             this.label8.Text = ".";
             // 
@@ -192,7 +190,7 @@
             this.label9.AutoSize = true;
             this.label9.Location = new System.Drawing.Point(453, 67);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(12, 17);
+            this.label9.Size = new System.Drawing.Size(10, 13);
             this.label9.TabIndex = 14;
             this.label9.Text = ".";
             // 
@@ -201,7 +199,7 @@
             this.label10.AutoSize = true;
             this.label10.Location = new System.Drawing.Point(453, 97);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(12, 17);
+            this.label10.Size = new System.Drawing.Size(10, 13);
             this.label10.TabIndex = 15;
             this.label10.Text = ".";
             // 
@@ -210,7 +208,7 @@
             this.label11.AutoSize = true;
             this.label11.Location = new System.Drawing.Point(453, 125);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(12, 17);
+            this.label11.Size = new System.Drawing.Size(10, 13);
             this.label11.TabIndex = 16;
             this.label11.Text = ".";
             // 
@@ -219,7 +217,7 @@
             this.label12.AutoSize = true;
             this.label12.Location = new System.Drawing.Point(9, 188);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(530, 170);
+            this.label12.Size = new System.Drawing.Size(400, 130);
             this.label12.TabIndex = 17;
             this.label12.Text = resources.GetString("label12.Text");
             // 
@@ -232,16 +230,6 @@
             this.button4.Text = "Ispis iz baze";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(642, 57);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(240, 51);
-            this.label13.TabIndex = 19;
-            this.label13.Text = "Provjera da li postoji zapis u bazi \r\nili nakon otvaranja csv- datoteke \r\nmože se" +
-    " vidjeti postojeći zapis u bazi";
             // 
             // button5
             // 
@@ -258,18 +246,9 @@
             this.label14.AutoSize = true;
             this.label14.Location = new System.Drawing.Point(666, 202);
             this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(195, 34);
+            this.label14.Size = new System.Drawing.Size(144, 26);
             this.label14.TabIndex = 21;
             this.label14.Text = "Izvlači samo financijska \r\nodobrenja za odabrani period";
-            // 
-            // label15
-            // 
-            this.label15.AutoSize = true;
-            this.label15.Location = new System.Drawing.Point(258, 13);
-            this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(291, 17);
-            this.label15.TabIndex = 22;
-            this.label15.Text = "Baza se briše prije učitavanja novog csv fajla";
             // 
             // button6
             // 
@@ -295,7 +274,7 @@
             // 
             this.txtDob.Location = new System.Drawing.Point(575, 262);
             this.txtDob.Name = "txtDob";
-            this.txtDob.Size = new System.Drawing.Size(141, 22);
+            this.txtDob.Size = new System.Drawing.Size(141, 20);
             this.txtDob.TabIndex = 25;
             // 
             // label16
@@ -303,7 +282,7 @@
             this.label16.AutoSize = true;
             this.label16.Location = new System.Drawing.Point(575, 240);
             this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(102, 17);
+            this.label16.Size = new System.Drawing.Size(77, 13);
             this.label16.TabIndex = 26;
             this.label16.Text = "Dobavljač-filter";
             // 
@@ -313,7 +292,7 @@
             this.datumOd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
             this.datumOd.Location = new System.Drawing.Point(100, 67);
             this.datumOd.Name = "datumOd";
-            this.datumOd.Size = new System.Drawing.Size(122, 22);
+            this.datumOd.Size = new System.Drawing.Size(122, 20);
             this.datumOd.TabIndex = 1;
             this.datumOd.Value = new System.DateTime(2019, 2, 13, 21, 5, 21, 0);
             // 
@@ -324,10 +303,8 @@
             this.Controls.Add(this.txtDob);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
-            this.Controls.Add(this.label15);
             this.Controls.Add(this.label14);
             this.Controls.Add(this.button5);
-            this.Controls.Add(this.label13);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.label11);
@@ -377,10 +354,8 @@
         protected System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Label label13;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Label label14;
-        private System.Windows.Forms.Label label15;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
         protected System.Windows.Forms.TextBox txtDob;
